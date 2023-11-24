b2e17bcd214e71f5e50c31a8969556cbdc1b120a
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-23 19:37:13 -0500

    Refactor database.ts to use ES6 import/export syntax

--- src/database.ts
+++ src/database.ts
@@ -1,29 +1,31 @@
-const {Sequelize, DataTypes} = require('sequelize');
+import { Sequelize, DataTypes } from 'sequelize';
 
 const sequelize = new Sequelize('chat-doc', 'root', 'my-secret-pw', {
-    host: 'localhost',
-    port: 3306,
-    dialect: 'mysql'
+  host: 'localhost',
+  port: 3306,
+  dialect: 'mysql',
 });
 
-const FileTable = sequelize.define('File', {
-    filename: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    guid: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      allowNull: false
-    },
-    fileData: {
-      type: DataTypes.BLOB('long'),
-      allowNull: false
-    }
+export const FileTable = sequelize.define('File', {
+  filename: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  guid: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    allowNull: false,
+  },
+  fileData: {
+    type: DataTypes.BLOB('long'),
+    allowNull: false,
+  },
 });
 
-FileTable.sync({ force: true }).then(() => {
+FileTable.sync({ force: true })
+  .then(() => {
     console.log('File table has been created.');
-}).catch(console.error);
+  })
+  .catch(console.error);
 
 module.exports = FileTable;
