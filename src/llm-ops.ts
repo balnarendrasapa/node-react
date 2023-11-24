861b5d6b59bedc38e53dcccf7db0a1273f23d623
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-24 17:39:57 -0500

    Remove unused code in llm-ops.ts

--- src/llm-ops.ts
+++ src/llm-ops.ts
@@ -22,25 +22,3 @@ export const getVectordb = async (file: Blob) => {
   return vectorStore;
 
 };
-
-export const getVectordbFromIndexDocstore = async (
-  docstore:any, 
-  index:any, 
-  mapping:any) => {
-  const FaissStore = (await FaissStoreM).FaissStore;
-  const HuggingFaceTransformersEmbeddings = (
-    await HuggingFaceTransformersEmbeddingsM
-  ).HuggingFaceTransformersEmbeddings;
-  const model = new HuggingFaceTransformersEmbeddings({
-    modelName: 'Xenova/all-MiniLM-L6-v2',
-  });
-  const vectorStore = new FaissStore(model, {docstore, index, mapping});
-  return vectorStore;
-}
-// const main = async () => {
-//   const vst = await getVectordb();
-//   console.log('--------------------');
-//   console.log(await vst.similaritySearch('geographical', 1));
-// };
-
-// main();
