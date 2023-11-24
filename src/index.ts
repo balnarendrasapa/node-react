ba940b4d98dada682b9f27536946776e11519e2e
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-24 17:54:55 -0500

    Refactor file upload and add chat functionality

--- src/index.ts
+++ src/index.ts
@@ -16,13 +16,6 @@ const app = express();
 const port = 3000;
 
 app.post('/upload', upload.single('file'), async (req, res) => {
-  const FaissStore = (await FaissStoreM).FaissStore;
-  const HuggingFaceTransformersEmbeddings = (
-    await HuggingFaceTransformersEmbeddingsM
-  ).HuggingFaceTransformersEmbeddings;
-  const model = new HuggingFaceTransformersEmbeddings({
-    modelName: 'Xenova/all-MiniLM-L6-v2',
-  });
 
   if (!req.file) {
     res.status(400).json({ message: 'No file uploaded' });
@@ -33,19 +26,8 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 
   const vdb = await getVectordb(new Blob([req.file.buffer]));
   await vdb.save("files/")
-  // const vdb2 = await FaissStore.load("files/", model)
-  // console.log(await vdb2.similaritySearch('geographical', 1));
-  // console.log("----------")
   const docstorejson = fs.readFileSync('files/docstore.json');
   const faissindex = fs.readFileSync('files/faiss.index');
-  // fs.writeFile('files/docstore1.json', docstorejson, (err:any) => {
-  //   if (err) throw err;
-  //   console.log('The file has been saved!');
-  // });
-  // fs.writeFile('files/faiss1.index', faissindex, (err:any) => {
-  //   if (err) throw err;
-  //   console.log('The file has been saved!');
-  // });
   const fileData = {
     filename: req.file.originalname,
     guid: guid,
@@ -59,6 +41,30 @@ app.post('/upload', upload.single('file'), async (req, res) => {
     .catch((err: any) => {
       res.status(500).json({ message: 'Error uploading file', error: err });
     });
+});
+
+app.post('/chat/:guid', async (req, res) => {
+  const FaissStore = (await FaissStoreM).FaissStore;
+  const HuggingFaceTransformersEmbeddings = (
+    await HuggingFaceTransformersEmbeddingsM
+  ).HuggingFaceTransformersEmbeddings;
+  const model = new HuggingFaceTransformersEmbeddings({
+    modelName: 'Xenova/all-MiniLM-L6-v2',
+  });
+
+  const guid = req.params.guid;
+  FileTable.findOne({ where: { guid: guid } }).then((file: any) => {
+    fs.writeFile('files/docstore.json', file.docstorejson, (err: any) => {
+      if (err) throw err;
+    });
+    fs.writeFile('files/faiss.index', file.faissindex, (err: any) => {
+      if (err) throw err;
+    });
+  })
+  const vdb = await FaissStore.load("files/", model)
+  console.log("------------------")
+  console.log(await vdb.similaritySearch('geographical', 1));
+  res.json({ message: 'Chat started successfully' });
 });
 
 app.get('/showfile/:guid', (req, res) => {
