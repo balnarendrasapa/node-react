0a13def2b3b0e1bfe18032f06f760836392629a6
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-11-24 15:51:12 -0500

    Refactor getVectordb function and add getVectordbFromIndexDocstore function

--- src/llm-ops.ts
+++ src/llm-ops.ts
@@ -4,7 +4,7 @@ const HuggingFaceTransformersEmbeddingsM = import(
   'langchain/embeddings/hf_transformers'
 );
 
-export const run = async () => {
+export const getVectordb = async (file: Blob) => {
   const FaissStore = (await FaissStoreM).FaissStore;
   const PDFLoader = (await PDFLoaderM).PDFLoader;
   const HuggingFaceTransformersEmbeddings = (
@@ -13,21 +13,34 @@ export const run = async () => {
   const model = new HuggingFaceTransformersEmbeddings({
     modelName: 'Xenova/all-MiniLM-L6-v2',
   });
-  const loader = new PDFLoader('./files/abc.pdf');
+  const loader = new PDFLoader(file);
 
   const vectorStore = await FaissStore.fromDocuments(
     await loader.load(),
     model,
   );
-  const resultOne = await vectorStore.similaritySearch('geographical', 1);
-  console.log(resultOne);
   return vectorStore;
+
 };
 
-const main = async () => {
-  const vst = await run();
-  console.log('--------------------');
-  console.log(await vst.similaritySearch('geographical', 1));
-};
+export const getVectordbFromIndexDocstore = async (
+  docstore:any, 
+  index:any, 
+  mapping:any) => {
+  const FaissStore = (await FaissStoreM).FaissStore;
+  const HuggingFaceTransformersEmbeddings = (
+    await HuggingFaceTransformersEmbeddingsM
+  ).HuggingFaceTransformersEmbeddings;
+  const model = new HuggingFaceTransformersEmbeddings({
+    modelName: 'Xenova/all-MiniLM-L6-v2',
+  });
+  const vectorStore = new FaissStore(model, {docstore, index, mapping});
+  return vectorStore;
+}
+// const main = async () => {
+//   const vst = await getVectordb();
+//   console.log('--------------------');
+//   console.log(await vst.similaritySearch('geographical', 1));
+// };
 
-main();
+// main();
